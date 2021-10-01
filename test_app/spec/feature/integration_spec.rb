7e5e9add5faee55b92c5a9bb66506a902c7f4a59
Ben Paul <benjabot97@gmail.com>
2021-09-30 22:36:41 -0500

    Almost Done

---
+++ test_app/spec/feature/integration_spec.rb
@@ -0,0 +1,18 @@
+# location: spec/feature/integration_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J. K. Rowling'
+    fill_in 'Price', with: '15.95'
+    fill_in 'publisheddate', with: '19970626'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter')
+    expect(page).to have_content('J. K. Rowling')
+    expect(page).to have_content('15.95')
+    expect(page).to have_content('19970626')
+  end
+end
---
+++ test_app/spec/unit/unit_spec.rb
@@ -0,0 +1,35 @@
+# location: spec/unit/unit_spec.rb
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter')
+    described_class.new(author: 'J. K. Rowling')
+    described_class.new(price: '15.95')
+    described_class.new(publisheddate: '1997-06-26')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a published date' do
+    subject.publisheddate = nil
+    expect(subject).not_to be_valid
+  end
+end
