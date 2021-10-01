8cc8c40af96dc586a298e496565770bcb7fc9c37
Ben Paul <benjabot97@gmail.com>
2021-09-30 23:27:05 -0500

    Trying

--- test_app/spec/feature/integration_spec.rb
+++ test_app/spec/feature/integration_spec.rb
@@ -7,10 +7,12 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'J. K. Rowling'
     fill_in 'Price', with: '15.95'
+    fill_in "Publisheddate", with: "1997-06-26"
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
     expect(page).to have_content('J. K. Rowling')
     expect(page).to have_content('15.95')
+    expect(page).to have_content("1997-06-26")
   end
 end
--- test_app/spec/unit/unit_spec.rb
+++ test_app/spec/unit/unit_spec.rb
@@ -4,13 +4,6 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   subject do
     described_class.new(title: 'harry potter')
-    described_class.new(author: 'J. K. Rowling')
-    described_class.new(price: '15.95')
-    described_class.new(publisheddate: '1997-06-26')
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
   end
 
   it 'is not valid without a name' do
@@ -18,9 +11,17 @@ RSpec.describe Book, type: :model do
     expect(subject).not_to be_valid
   end
 
+  subject do
+    described_class.new(author: 'J. K. Rowling')
+  end
+
   it 'is not valid without an author' do
     subject.author = nil
     expect(subject).not_to be_valid
+  end
+
+  subject do
+    described_class.new(price: '15.95')
   end
 
   it 'is not valid without a price' do
@@ -28,5 +29,14 @@ RSpec.describe Book, type: :model do
     expect(subject).not_to be_valid
   end
 
+  subject do
+    described_class.new(publisheddate: '1997-06-26')
+  end
+
+
+  it 'is not valid without a published date' do
+    subject.publisheddate = nil
+    expect(subject).not_to be_valid
+  end
 
 end
