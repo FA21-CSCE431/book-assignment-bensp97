7e3fb374c02a503b7f24790318ff46dd2e66504f
Ben Paul <benjabot97@gmail.com>
2021-09-30 20:54:06 -0500

    Created Testing for the other attributes

--- test_app/spec/feature/integration_spec.rb
+++ test_app/spec/feature/integration_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J. K. Rowling'
+    fill_in 'Price', with: '15'
+    fill_in 'Published Date', with: '19970626'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
--- test_app/spec/unit/unit_spec.rb
+++ test_app/spec/unit/unit_spec.rb
@@ -4,6 +4,9 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   subject do
     described_class.new(title: 'harry potter')
+    described_class.new(author: 'J. K. Rowling')
+    described_class.new(price: '15')
+    described_class.new(publisheddate: '19970626')
   end
 
   it 'is valid with valid attributes' do
@@ -12,6 +15,21 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a published date' do
+    subject.publisheddate = nil
     expect(subject).not_to be_valid
   end
 end
